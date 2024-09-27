4985a13966812e2e65fa7d0cd7b174f101a70782
SpaceShaman <spaceshaman@tuta.io>
2024-09-27 14:13:45 +0200

    Added CV

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxtjs/color-mode"],
+  ssr: false,
+  hooks: {
+    "prerender:routes"({ routes }) {
+      routes.clear(); // Do not generate any routes (except the defaults)
+    },
+  },
+  app: {
+    baseURL: "/cv/",
+  },
+});
